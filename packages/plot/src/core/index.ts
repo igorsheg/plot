0459346c7adb009f1db404d2839ff56ced04755c
Igor Sheg <igorsheg@gmail.com>
2026-03-18 13:29:19 +0200

    feat(plot): extract GitFlow as a strategy pattern
    Session-Id: 52705e94-6e27-47ba-8a52-da103ee4dd65

--- packages/plot/src/core/index.ts
+++ packages/plot/src/core/index.ts
@@ -3,3 +3,6 @@ export { ResolvedConfig, validateForDispatch } from "./config-service.js";
 export { WorkspaceManager } from "./workspace-manager.js";
 export { renderPrompt } from "./prompt-renderer.js";
 export { Orchestrator } from "./orchestrator.js";
+export { GitFlow, GitFlowError } from "./git-flow.js";
+export { GitFlowPR } from "./git-flow-pr.js";
+export { GitFlowCommit } from "./git-flow-commit.js";
